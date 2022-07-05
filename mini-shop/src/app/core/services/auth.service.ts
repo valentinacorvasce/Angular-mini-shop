5a1c2fa55ef4b2e1dff134b1b7b08a1293d34061
Valentina <lilithsixx@gmail.com>
2022-07-05 20:59:58 +0200

    bug: token not obtained: undefined

--- mini-shop/src/app/core/services/auth.service.ts
+++ mini-shop/src/app/core/services/auth.service.ts
@@ -1,41 +1,83 @@
-import { HttpClient, HttpParams } from '@angular/common/http';
+import { HttpClient, HttpErrorResponse, HttpHeaders, HttpParams } from '@angular/common/http';
 import { Injectable } from '@angular/core';
+import { NgForm } from '@angular/forms';
 import { Router } from '@angular/router';
+import { catchError, map, Observable, throwError } from 'rxjs';
 import { Auth } from 'src/app/models/auth';
 
-const url = `http://localhost:3000/login`;
+const url = `http://localhost/php-auth-api/login.php`;
 
 @Injectable({
   providedIn: 'root'
 })
 export class AuthService {
   data!: Auth | any;
+  private options: HttpHeaders = new HttpHeaders().set('Content-type', 'application/json');
 
   constructor(private http: HttpClient, private router: Router) { }
 
-  login(form: any): any {
+  login(form: NgForm): Observable<string> {
+    const body = this.body(form);
 
+    return this.http.post(url, body, { headers: this.options })
+      .pipe(
+        map(res => {
+          this.data = res;
+          if (this.data['token']) {
+            this.setSession(this.data['token']);
+          }
+          return this.data['token'];
+        }),
+        catchError(this.errorHandler)
+      );
+
+  }
+
+  private body(df: NgForm) {
     const params = new HttpParams()
-      .set('email', form.email)
-      .set('password', form.pass)
-
-    this.http.get<Auth>(url, { params })
-      .subscribe(res => {
-        this.data = res;
-        console.log(this.data);
-        this.router.navigateByUrl('home');
-      })
-
+      .set('email', df.value.email)
+      .set('password', df.value.pass)
+    return params;
   }
 
   logout(): any {
     this.data = null;
     this.router.navigateByUrl('login');
+    localStorage.removeItem('token');
+    localStorage.removeItem('expired');
   }
 
   isLogged(): any {
     const isAuth = this.data && this.data.token ? true : false;
     return isAuth;
+  }
+
+  setSession(jwt: string) {
+    let expire: number = new Date().getTime() + 10000;
+    localStorage.setItem('token', jwt);
+    localStorage.setItem('expired', expire.toString());
+  }
+  notExpired(): boolean {
+    if (localStorage.getItem('expire')) {
+      let expire: number = parseInt(localStorage.getItem('expire') as string);
+      return new Date().getTime() < expire;
+    }
+    return false;
+  }
+
+  /* Gestione Errori */
+  errorHandler(error: any) {
+    console.log(error);
+    let msg: string;
+    if (error instanceof HttpErrorResponse) {
+      if (error.status === 0) {
+        msg = `Applicazione Offline!`
+      } else {
+        msg = `Si è verificato un errore: ${error.error.msg} (server status code ${error.status})`;
+      }
+      return throwError(msg);
+    }
+    return throwError(`Si è verificato un errore di tipo: ${error.message}`);
   }
 
 
--- mini-shop/src/app/features/login/login.component.ts
+++ mini-shop/src/app/features/login/login.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import { NgForm } from '@angular/forms';
+import { Router } from '@angular/router';
 import { AuthService } from 'src/app/core/services/auth.service';
 
 @Component({
@@ -7,10 +9,24 @@ import { AuthService } from 'src/app/core/services/auth.service';
   styleUrls: ['./login.component.css']
 })
 export class LoginComponent implements OnInit {
+  modelEmail!: string;
+  modelPass!: string;
+  showMsg!: string;
 
-  constructor(public auth: AuthService) { }
+  constructor(public auth: AuthService, private router: Router) { }
 
   ngOnInit(): void {
+  }
+
+  sendLogin(form: NgForm) {
+    this.auth.login(form)
+      .subscribe(res => {
+        alert('Il token passato è: ' + res)
+        this.router.navigateByUrl('book')
+      },
+        error => this.showMsg = error
+      )
+
   }
 
 }
